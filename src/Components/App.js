f12fb540dd9d94db929e5744d5389b201c3e3777
Sam Knowles <samuelkcknowles@gmail.com>
2018-11-11 18:14:06 -0700

    added a background image

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from './Home';
+
+import '../Styles/Home.css';
+
+function App() {
+    return (
+        <div className='app-wrapper'>
+            <Home />
+        </div>
+    )
+}
+
+export default App
