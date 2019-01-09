cc9ef0f22e731e1e09264d3bd59cc80646c9ee48
Sam Knowles <samuelkcknowles@gmail.com>
2019-01-09 16:04:09 -0700

    added styling to main page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
+
 function Home() {
     return (
         <div className='home-wrapper'>
                 <div className="left-box">
                     <div className='left-center'>
-                        <div>Coming Soon</div>
-                        <div>Contact us</div>
-                        <div>Come find us</div>
+                        <div className='top-box'>Coming Soon</div>
+                        <div className='middle-box'>Contact us</div>
+                        <div className='bottom-box'>Come find us</div>
                     </div>
                 </div>
                 <div className="right-box">2</div>
